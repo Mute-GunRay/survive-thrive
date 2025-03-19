d43bb13f32f5dcc0e20517e7fec50554cecb6a26
Tate Ray <tate@tateray.com>
2025-03-19 09:39:13 -0600

    initial js;

---
+++ helpers.js
@@ -0,0 +1,26 @@
+function timer(time, timer){
+    let timeRemaining = time;
+    let interval = setInterval(() => {
+	timer = timeRemaining - 1000;
+	displayTime(timeRemaining, timer)
+	if(timeRemaining < 0){
+	    clearInterval(interval);
+	}
+    }, 1000)
+}
+
+function displayTime(time, timer){
+    timer[0].textContent =  Math.floor(time / (1000 * 60 * 60 * 24));
+    timer[1].textContent = Math.floor((time % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+    timer[2].textContent = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    timer[3].textContent = Math.floor((time % (1000 * 60)) / 1000)
+}
+
+function calculateTime(level, labor, tier){
+    let max = (level + tier) % (labor % 2);
+    return generateRandomValue(max);
+}
+
+function getTimer(){
+    
+}
