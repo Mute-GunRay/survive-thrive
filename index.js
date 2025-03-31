ab7b29d0f5420a2ce505ba4b1e898d0a1e789e67
Tate Ray <tate@tateray.com>
2025-03-31 10:45:30 -0600

    ...;

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ function tickHour(){
     document.getElementById(`task-${currentHour}`).disabled = true;
 }
 
-setInterval(1000, tickMinute);
-setInterval(60000, tickHour);
+setInterval(1000, tickMinute());
+setInterval(60000, tickHour());
