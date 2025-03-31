94addeb4394054fc7c24c16bd92b9a5827434880
Tate Ray <tate@tateray.com>
2025-03-31 10:23:27 -0600

    added current time and selects for tasks; added interval callbacks;

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+let currentTime = document.getElementById('current-time');
+let currentMinute = 0
+let currentHour = 0;
 
 
-function loadGame(){
-    gameData = JSON.parse(localStorage.getItem('thriveData'));
-
-    if(gameData){
-	
+function tickMinute(){
+    if (currentMinute == 59) {
+	currentMinute = 0;
     } else {
-
+	currentMinute = currentMinute + 1;
+    }
+    
+    if (currentHour < 10 && currentMinute < 10) {
+	currentTime.textContent = `0${currentHour}:0${currentMinute}`;
+    } else {
+	currentTime.textContent = `${currentHour}:${currentMinute}`;
     }
 }
 
-loadGame();
+function tickHour(){
+    document.getElementById(`task-${currentHour}`).disabled = false
+
+    if (currentHour == 23) {
+	currentHour = 0
+    }
+
+    currentHour = currentHour + 1;
+
+    document.getElementById(`task-${currentHour}`).disabled = true;
+}
+
+
+setInterval(60000, tickHour);
+setInterval(1000, tickMinute);
