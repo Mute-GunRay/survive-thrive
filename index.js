95f0cf4d009d4f225dfca0c727c77d509ab05f6c
Tate Ray <tate@tateray.com>
2025-04-08 10:49:09 -0600

    added block for stat trinity; added skills variable;

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ let player = {
     'energy modifier': 0.1,
     'hunger modifier': 0.1,
     'thirst modifier': 0.1,
+    'mind': [0, 0, 100],
+    'body': [0, 0, 100],
+    'spirit': [0, 0, 100],
 }
 
 let schedule = [
@@ -62,12 +65,37 @@ let schedule = [
     ]
 ]
 
+// what are some basic skills that fit within the setting of this game?
+let skills = [
+    [ // these are social skills
+	'charm', 'intimidate', 'calm', 'haggle', 'communication',
+	'leadership', 'empathy', 'listening',
+    ],
+    [ // these are tech skills
+	'programming', 'network architecture', 'data mining', 'data analytics',
+	'network security', 'circuit design', 
+    ],
+    [ // these are fighting skills
+	'unarmed', 'evasion', 'small arms', 'rifles', 'heavy guns',
+	'small blades', 'straight blades', 'curved blades', 'shields',
+	'small bows', 'large bows', 'heavy bows', 
+    ],
+    [ // these are knowledge skills
+	'reading', 'writing', 'comprehension', 'recall', 'memory', 'observation',
+	'logic', 'discipline', 'research'
+    ],
+    [ // these are magic skills
+	'casting', 'theory', 'alchemy', 'spell crafting', 'elemental',
+	'psychic', 'mana control', 'astrology', 'augury', 
+    ],
+    [ // these are physical skills
+	'weight lifting', 'running', 'flexibility', 'balance',
+	'corrdination', 'endurance',
+    ],
+]
+
 function tickMinute(){
-    if (currentMinute == 59) {
-	currentMinute = 0;
-    } else {
-	currentMinute = currentMinute + 1;
-    }
+    currentMinute = currentMinute == 59 ? 0 : currentMinute + 1
     
     tickResource('energy', 0.01);
     tickResource('hunger', 0.01);
@@ -80,15 +108,11 @@ function tickMinute(){
 }
 
 function tickHour(){
-    document.getElementById(`task-${currentHour}`).disabled = false
+    document.getElementById(`task-${currentHour}`).disabled = false;
 
     if (currentHour == 23) {
 	currentHour = 0;
-	if ( currentDay < 6) {
-	    currentDay = currentDay + 1;
-	} else {
-	    currentDay = 0;
-	}
+	currentDay = currentDay < 6 ? currentDay + 1 : 0
 	setDailySchedule();
     } else {
 	currentHour = currentHour + 1;
@@ -108,12 +132,13 @@ function populateTaskOptions(){
 }
 
 function setDailySchedule() {
+    // test that selected day changes properly
+    // suspect that it does not
     let dailySchedule = schedule[currentDay];
     let days = Array.from(document.getElementById('days').children);
     days[currentDay].selected = true;
     taskSelects.forEach((hour, index) => {
-	let options = Array.from(hour.children);
-	options.forEach((option) => {
+	Array.from(hour.children).forEach((option) => {
 	    if (option.textContent == dailySchedule[index]) {
 		option.selected = true;
 		return;
@@ -127,21 +152,9 @@ function tickResource(resource, amount){
 }
 
 function formatCurrentTime(){
-    let formattedMinute;
-    let formattedHour;
-
-    if(currentMinute < 10){
-	formattedMinute = `0${currentMinute}`;
-    } else {
-	formattedMinute = currentMinute.toString();
-    }
-    if(currentHour < 10){
-	formattedHour = `0${currentHour}`;
-    } else {
-	formattedHour = currentHour.toString();
-    }
-
-    return `${formattedHour}:${formattedMinute}`;
+    let minute = currentMinute < 10 ? `0${currentMinute}` : currentMinute.toString();
+    let hour = currentHour < 10 ? `0${currentHour}` : currentHour.toString();
+    return `${hour}:${minute}`;
 }
 
 function updateUI(element, value){
@@ -149,10 +162,13 @@ function updateUI(element, value){
 }
 
 function start() {
-    document.getElementById('health').textContent = player['health'][0];
-    document.getElementById('energy').textContent = player['energy'][0];
-    document.getElementById('hunger').textContent = player['hunger'][0];
-    document.getElementById('thirst').textContent = player['thirst'][0];
+    updateUI('health', player['health'][0]);
+    updateUI('energy', player['energy'][0]);
+    updateUI('hunger', player['hunger'][0]);
+    updateUI('thirst', player['thirst'][0]);
+    updateUI('mind', player['mind'][0]);
+    updateUI('body', player['body'][0]);
+    updateUI('spirit', player['spirit'][0]);
     populateTaskOptions();
     setDailySchedule();
 }
