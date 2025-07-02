a7c1f1a3e9c14a8fe18eef368bda5335480c9cac
Tate Ray <tate@tateray.com>
2025-07-02 11:08:16 -0600

    forgot index;

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ document.getElementById("thirst").textContent = player.thirst[0]
 
 function tickMinute(){
     currentMinute == 59 ? currentMinute = 0 : currentMinute++;
-    player.energy = player.energy - 1;
+    player.energy[0] = player.energy[0] - 1;
     console.log(player.energy)
 }
 
