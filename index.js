75b5b594da4522917703fb96174ef1143ea03b6d
Tate Ray <tate@tateray.com>
2025-08-06 10:42:15 -0600

    added skill catagories; added skills as uls;

--- index.js
+++ index.js
@@ -14,12 +14,53 @@ document.getElementById("observation_number").textContent = player.observation;
 
 function renderSkills(){
     let skill_keys = Object.keys(skills);
-    let skills_block = document.getElementById('skills');
+    let social_skills_block = document.getElementById("social")
+    let tech_skills_block = document.getElementById("tech")
+    let combat_skills_block = document.getElementById("combat")
+    let knowledge_skills_block = document.getElementById("knowledge")
+    let magic_skills_block = document.getElementById("magic")
+    let physical_skills_block = document.getElementById("physical")
+    let social_skills_list = document.createElement('ul')
+    let tech_skills_list = document.createElement('ul')
+    let combat_skills_list = document.createElement('ul')
+    let knowledge_skills_list = document.createElement('ul')
+    let magic_skills_list = document.createElement('ul')
+    let physical_skills_list = document.createElement('ul')
+    
     skill_keys.forEach(skill_key => {
-	let skill_shard = document.createElement('span');
-	skill_shard.textContent = skills[skill_key]["name"]
-	skills_block.appendChild(skill_shard);
+	let skill_shard = document.createElement('li');
+	let skill_name_shard = document.createElement('span')
+	let skill_level_shard = document.createElement('span')
+	let skill = skills[skill_key]
+
+	skill_name_shard.textContent = skill['name']
+	skill_level_shard.textContent = skill['level_current']
+
+	skill_shard.appendChild(skill_name_shard);
+	skill_shard.appendChild(skill_level_shard);
+	
+	switch(skill['catagory']){
+	case "physical":
+	    physical_skills_list.appendChild(skill_shard);
+	case "social":
+	    social_skills_list.appendChild(skill_shard);
+	case "tech":
+	    tech_skills_list.appendChild(skill_shard)
+	case "combat":
+	    combat_skills_list.appendChild(skill_shard)
+	case "knowledge":
+	    knowledge_skills_list.appendChild(skill_shard)
+	default:
+	    magic_skills_list.appendChild(skill_shard)
+	}
     })
+
+    social_skills_block.appendChild(social_skills_list)
+    tech_skills_block.appendChild(tech_skills_list)
+    combat_skills_block.appendChild(combat_skills_list)
+    knowledge_skills_block.appendChild(knowledge_skills_list)
+    magic_skills_block.appendChild(magic_skills_list)
+    physical_skills_block.appendChild(physical_skills_list)
 }
 
 function tickMinute(){
